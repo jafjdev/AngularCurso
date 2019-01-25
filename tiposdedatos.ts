a802b0351efd24337be2b7181f2dec32043e5891
Jose Cedeño <josejecr97@gmail.com>
2019-01-25 14:17:46 -0800

    tipos de datos
    explicacion de tipos de datos.

---
+++ tiposdedatos.ts
@@ -0,0 +1,30 @@
+//Tipos de datos en Typescript
+
+//string: tipo texto
+
+let texto:string;
+texto = "hola soy un texto";
+
+//number: numericos
+
+let numero:number;
+numero = 123;
+
+//boolean T o F
+
+let bool:boolean;
+bool = true;
+bool = false;
+
+//any :cualquier cosa
+
+let cual:any;
+cual = "test";
+cual = 0;
+cual = bool;
+
+//string[]: array de caracteres, number[]: array de numeros
+//any[]: array de cualquier cosa
+
+let arrayCaracter: string[];
+arrayCaracter = ["xd","hola","test"];
