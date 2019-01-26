872493cd5d319a512dc6efd0bd62d4ade327890c
Jose Cedeño <josejecr97@gmail.com>
2019-01-25 17:49:32 -0800

    ngIf

--- Angular/my-app/src/app/app.module.ts
+++ Angular/my-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
